96abb4c3e70865ac2acc6406dbd8ef77ca991316
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-09-03 16:24:48 +0000

    added comments to file challenge.py to describe solution

--- challenge.py
+++ challenge.py
@@ -1,13 +1,28 @@
-# Open the file first "triangle.txt" and 
-# create a new list file_list with data
+"""
+This file is one solution for the triangle challenge. 
 
+The objective is to find the maximum total from top to bottom in
+one triangle containing 100 rows. 
+The constraint is to move only to adjacent numbers on the row below.
+
+Run the file with the command: 'python3 challenge.py' in the console
+to obtain an answer.
+"""
+
+# Open the file first "triangle.txt"
 txt_file = open("triangle.txt","r") 
+
+# Initilize new list with triangle values
 triangle = []
+
 for line in txt_file.readlines():
+    # separate items in every blank space
     line_list = line.split(' ')
+    # remove the \n sign from every line
     if '\n' in line_list:
         line_list.remove('\n')
     int_list = []
+    # create integer values and add them to triangle list
     for item in line_list:
         int_list.append(int(item))
     triangle.append(int_list)
@@ -22,16 +37,19 @@ for i in reversed(range(Num)):
     # Define length of each line
     j = len(triangle[i])
     for j in reversed(range(j)):
-        # Compare the values in adjacent pairs backwards, check for the larger value
+        # Compare the values in adjacent pairs backwards, 
+        # check for the larger value
         if triangle[i][j] > triangle[i][j-1]:
-            # Add the larger value to the corresponding adjacent value from previous line
+            # Add the larger value to the corresponding 
+            # adjacent value from previous line
             triangle[i-1][j-1] += triangle[i][j]
         else:
             triangle[i-1][j-1] += triangle[i][j-1]
 
-# Maximum value is added and replaced on top of triangle, thus the solution
+# Maximum value is added and replaced on top of triangle, 
+# thus the solution
 max_value = triangle[0][0]
-# Add 1000 separator commas to max value
+# Add 1000-separating commas to max value for formatting
 max_value_str = f"{max_value:,d}"
 
 # Print solution to console
